675bb077ed0ab5c7dc9fa1e18f5bf956d7ab04ba
amineminoop <aminemino12020@gmail.com>
2022-07-28 17:04:19 +0100

    kmlt

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
                     </div>
                     <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
                         <div className='p-4'>
-                            <form method='post' onSubmit={handleOnSubmit}>
+                            <form method='post' onSubmit={handleOnSubmit} data-netlify="true">
                                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                                     <div className='flex flex-col'>
                                         <label className='uppercase text-sm py-2'>Name</label>
